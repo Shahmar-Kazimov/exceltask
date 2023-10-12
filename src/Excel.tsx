408d0b0e55a8aed7c00682f54b6ec9d38536642f
Shahmar Kazimov <shahmar.kazimoff@gmail.com>
2023-10-12 15:48:15 +0400

    Sorting and Filtering functions added

--- src/Excel.tsx
+++ src/Excel.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react"
 import * as XLSX from "xlsx"
+
 function Excel() {
-    const [excelFile, setExcelFile] = useState(null);
+    const [excelFile, setExcelFile] = useState("");
     const [typeError, setTypeError] = useState("");
-    const [excelData, setExcelData] = useState([]);
+    const [excelData, setExcelData] = useState<any[]>([]);
+    const [filters, setFilters] = useState<any>({})
+    const [filteredData, setFilteredData] = useState<any[]>([])
+    const [sortOrder, setSortOrder] = useState("asc");
     const handleFile = (e: any) => {
         let fileTypes = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
         let selectedFile = e.target.files[0];
@@ -18,7 +22,7 @@ function Excel() {
             }
             else {
                 setTypeError("Please select only excel file types")
-                setExcelFile(null)
+                setExcelFile("")
             }
         }
         else {
@@ -26,15 +30,62 @@ function Excel() {
         }
     }
 
+    const handleFilterInputChange = (headerKey: string, value: string) => {
+        var newFilters = { ...filters, [headerKey]: value }
+        setFilters(newFilters)
+        console.log(newFilters)
+
+        var newFilteredData = excelData.filter(row => {
+            var filterLogic: boolean = true
+            Object.keys(newFilters).map((key) => {
+                if (newFilters[key]) {
+                    filterLogic = filterLogic && row && row[key] && ((row[key] + "").indexOf(newFilters[key] + "") > -1)
+                }
+            })
+            console.log(filterLogic)
+            return filterLogic
+        })
+        // check filters
+        console.log("search result: ", newFilteredData.length)
+        setFilteredData(newFilteredData)
+    }
+
+    const handleSort = (headerKey: any) => {
+        const sortedData = filteredData.sort((a, b) => {
+            if (sortOrder === "asc") {
+                return a[headerKey] < b[headerKey] ? -1 : 1;
+            } else {
+                return a[headerKey] > b[headerKey] ? -1 : 1;
+            }
+        });
+        setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+        setFilteredData(sortedData);
+    };
+
     const handleFileSubmit = (e: any) => {
         e.preventDefault();
         if (excelFile !== null) {
             const workbook = XLSX.read(excelFile, { type: "buffer" });
             const worksheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[worksheetName];
-            var data: never[] = XLSX.utils.sheet_to_json(worksheet);
-            setExcelData(data.sort((a: { id: number }, b: { id: number }) => b.id - a.id));
+            var data: any[] = XLSX.utils.sheet_to_json(worksheet);
+            var sortedData: any[] = data.sort((a, b) => (b.id || 0) - (a.id || 0))
+            setExcelData(sortedData);
+            setFilteredData(sortedData)
         }
+    }
+
+    const renderFilteredData = () => {
+        console.log("render filtered data called")
+        return filteredData.map((individualExcelData: any, index: any) => {
+            return (
+                <tr key={index}>
+                    {Object.keys(individualExcelData).map((key) => (
+                        <td key={key}>{individualExcelData[key]}</td>
+                    ))}
+                </tr>
+            )
+        })
     }
 
     return (
@@ -48,7 +99,7 @@ function Excel() {
                 )}
             </form><br />
             <div>
-                {excelData ? (
+                {filteredData ? (
                     <div className="table responsive">
                         <table className="table table-bordered">
                             <thead>
@@ -56,19 +107,21 @@ function Excel() {
                                     {Object.keys(excelData && Array.isArray(excelData) && excelData.length > 0 ? excelData[0] : []).map((key, index) =>
                                         <th key={index}>
                                             {key}
-                                            <input type="text" placeholder="ara" />
+                                            <button onClick={() => handleSort(key)}>
+                                                {sortOrder === "asc" ? "▲" : "▼"}
+                                            </button>
+                                            <input
+                                                type="text"
+                                                placeholder="ara"
+                                                value={filters[key] || ""}
+                                                name={key}
+                                                onChange={(e) => handleFilterInputChange(key, e.target.value)} />
                                         </th>
                                     )}
                                 </tr>
                             </thead>
                             <tbody>
-                                {excelData.map((individualExcelData: any, index: any) => (
-                                    <tr key={index}>
-                                        {Object.keys(individualExcelData).map((key) => (
-                                            <td key={key}>{individualExcelData[key]}</td>
-                                        ))}
-                                    </tr>
-                                ))}
+                                {renderFilteredData()}
                             </tbody>
                         </table>
                     </div>
